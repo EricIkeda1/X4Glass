f95c4985767ff080fdf7335c173b72b00da77521
ikedayuji <ikedayuji.2002@gmail.com>
2024-09-11 11:07:39 -0300

    frontend em desenvolvimento

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Grid from './components/Grid';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Grid />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="../images/Temperlandia Logo.png" alt="Logo" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
+
+  
